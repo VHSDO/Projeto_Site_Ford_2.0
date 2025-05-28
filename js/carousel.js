114f6d4b61ccc5935aed5feb10c097644e52b9d4
VHSDO <santosdeoliveiravictorhugo1@gmail.com>
2025-05-28 16:06:26 -0300

    Foi adicionado o carrossel de imagens na página home do projeto junto com a funcionalidade de passar as imagens pelo botão

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,51 @@
-
-
-//carousel
-
-//Array storage class
 let carouselArr = [];
 
-
-//class Carousel
 class Carousel {
+    constructor(imagem, titulo, link){
+        this.imagem = imagem;
+        this.titulo = titulo;
+        this.link = link;    
+    }
 
-    
-      
     static Start(arr){
-        if(arr){
-
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
+        if (!arr || arr.length === 0){
+            throw "Você precisa fornecer um array com imagens.";
         }
+    
+        Carousel._sequence = 0;
+        Carousel._size = arr.length;
+        Carousel._interval = setInterval(() => Carousel.AutoNext(), 3000); // muda automaticamente a cada 3 segundos
+        Carousel.Next(); // mostra primeira imagem
     }
 
-    static Next(){
-        
+    static Next() {
+        let container = document.getElementById("carrossel");
+        let title = document.getElementById("titulo-carrossel");
+        let item = carouselArr[Carousel._sequence];
+
+        container.innerHTML = `
+            <button onclick="voltarImagem()"> < </button>
+            <a href="${item.link}">
+                <img src="img/${item.imagem}" alt="${item.titulo}">
+            </a>
+            <button onclick="passarImagem()"> > </button>
+        `;
+
+        title.innerText = item.titulo;
     }
-};
+
+    static AutoNext() {
+        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+        Carousel.Next();
+    }
+}
+
+function passarImagem() {
+    Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+    Carousel.Next(); 
+}
+
+function voltarImagem() {
+    Carousel._sequence = (Carousel._sequence - 1 + Carousel._size) % Carousel._size;
+    Carousel.Next(); 
+}
