0a1a651d1834150173a9a2e89006bfa72c47f528
VHSDO <santosdeoliveiravictorhugo1@gmail.com>
2025-06-03 13:26:52 -0300

    Foi posto os links da ford nas páginas lancamento e contato, alem de fazer a interação do alert na página contato.

--- js/form.js
+++ js/form.js
@@ -10,18 +10,15 @@ function Post(form) {
   let data = new contato
     (
         form.elements.namedItem("nome").value,
-        form.elements.namedItem("sobrenome").value, 
         form.elements.namedItem("email").value, 
-        form.elements.namedItem("cpf").value, 
         form.elements.namedItem("telefone").value, 
-        form.elements.namedItem("contato").value
     );
   
 }
 
 function Enviar() {
 
-    var nome = document.getElementById("nomeid");
+    var nome = document.getElementById("nome");
 
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
